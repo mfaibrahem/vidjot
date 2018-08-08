4943056499e2c9fcdf9752b8b83d2a5bd444ebdf
mfaibrahem <mfaibrahem2013@gmail.com>
2018-08-08 15:31:25 +0200

    added registeration and login forms and routes, create a logo

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/register', (rea, res) => {
-  res.send('register');
+  res.render('./users/register');
 });
 
 router.get('/login', (req, res) => {
